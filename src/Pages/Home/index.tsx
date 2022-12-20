c22b30468392a8373ee9e2ab006706a093b8e0a7
unknown <gabers20000@gmail.com>
2022-12-19 23:29:59 -0300

    feat: CardPerfilAdm e header fixado na pagina.

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -5,6 +5,8 @@ import { Container } from "./style";
 import { TokenContext } from "../../Providers/Token";
 import { Footer } from "../../Component/Footer";
 import { Header } from "../../Component/Header";
+import { CardPerfilAdm } from "../../Component/CardPerfilAdm";
+
 
 export const Home = () => {
   const { token } = useContext(TokenContext);
@@ -14,23 +16,24 @@ export const Home = () => {
   }, [token]);
 
   return (
-    <Container>
+    <>
       <Header></Header>
-      <div className="teste">
-        <p>Conteudo teste</p>
-        <p>Conteudo teste</p>
-        <p>Conteudo teste</p>
-        <p>Conteudo teste</p>
-        <p>Conteudo teste</p>
-        <p>Conteudo teste</p>
-        <p>Conteudo teste</p>
-        <p>Conteudo teste</p>
-        <p>Conteudo teste</p>
-        <p>Conteudo teste</p>
-        <p>Conteudo teste</p>
-        <p>Conteudo teste</p>
-      </div>
-      <Footer></Footer>
-    </Container>
+      <Container>
+        <CardPerfilAdm />
+        <div className="teste">
+          <p>Conteudo teste</p>
+          <p>Conteudo teste</p>
+          <p>Conteudo teste</p>
+          <p>Conteudo teste</p>
+          <p>Conteudo teste</p>
+          <p>Conteudo teste</p>
+          <p>Conteudo teste</p>
+          <p>Conteudo teste</p>
+          <p>Conteudo teste</p>
+          <p>Conteudo teste</p>
+        </div>
+        <Footer></Footer>
+      </Container>
+    </>
   );
 };
