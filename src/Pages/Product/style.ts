bc29d50fc1f7bdd76eb13d41227e34a27e7fa266
DavidBassouto <davidbassouto@hotmail.com>
2023-01-03 15:34:32 -0300

    fix: changes to avoid the layout to be broken and adition of more exemple of photos to test diferent sizes of image

--- src/Pages/Product/style.ts
+++ src/Pages/Product/style.ts
@@ -187,8 +187,9 @@ export const ContainerImages = styled.div`
     flex-wrap: wrap;
     margin: 0 auto;
 
-    width: 80.05698006%;
-    min-width: 17.5625rem;
+    width: 80%;
+
+    min-width: 17.6rem;
     .images--title {
       margin-top: 2.25rem;
       margin-left: 2.75rem;
@@ -196,10 +197,12 @@ export const ContainerImages = styled.div`
     figure {
       width: 32%;
       min-width: 5.625rem;
+      height: 5.15rem;
       &:hover {
-        transition: 0.7s;
-        background-color: var(--brand2);
+        transition: 0.2s;
+        border-bottom:0.2rem solid var(--brand2);
         cursor: pointer;
+        
       }
       img {
         max-width: 100%;
