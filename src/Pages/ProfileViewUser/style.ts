b13158da016c5e537a63fb40905de99ea5606ee2
abner-leals <abner.leal.silva@gmail.com>
2023-01-13 08:27:50 -0300

    fix: correção de layout do dashboard

--- src/Pages/ProfileViewUser/style.ts
+++ src/Pages/ProfileViewUser/style.ts
@@ -8,15 +8,30 @@ export const ContainerProfileUser = styled.div`
 
   width: 100%;
   max-width: 100vw;
-  background: linear-gradient(180deg, var(--brand1) 20.668rem, var(--grey8) 20.668rem);
-  padding: 9.688rem 0px 4.688rem 3.781rem;
+  background: linear-gradient(
+    180deg,
+    var(--brand1) 20.668rem,
+    var(--grey8) 20.668rem
+  );
+  padding: 9.688rem 0px 4.688rem 0.75rem;
 
   .containerCardPerfilAdm {
-    padding-right: 3.781rem;
+    width: 90%;
+    max-width: 77.5rem;
+    margin-left: auto;
+    margin-right: auto;
   }
 
-  @media(width>768px){
+  @media (width>768px) {
+    padding: 9.688rem 0px 4.688rem 3.781rem;
     align-items: center;
+    .containerCardPerfilAdm {
+      padding-right: 60.496px;
+      width: 77.5%;
+      max-width: 77.5rem;
+      margin-left: auto;
+      margin-right: auto;
+    }
   }
 `;
 
@@ -31,7 +46,6 @@ export const ContainerProductPerfil = styled.div`
 
   background-color: var(--grey8);
 
-
   .typeTittle {
     font-weight: 600;
     font-size: 1.5rem;
@@ -43,7 +57,7 @@ export const ContainerProductPerfil = styled.div`
   .product {
     margin-top: 1.438rem;
   }
-  
+
   .auction {
     display: none;
   }
